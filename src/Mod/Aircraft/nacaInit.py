5dedabc41545f0a5abd2282e13a9e86ccb1551ad
Jaime García <elgambitero@users.noreply.github.com>
2015-04-23 01:16:53 +0200

    External sources temporaly spared from watched files.

--- src/Mod/Aircraft/nacaInit.py
+++
@@ -1,8 +0,0 @@
-import numpy
-ides=7312
-xx=numpy.array([])
-yt=numpy.array([])
-yc=numpy.array([])
-xb=numpy.array([])
-yb=numpy.array([])
-nb=40
--- src/Mod/Aircraft/nacawrap.py
+++
@@ -1,4 +0,0 @@
-from numpy import f2py
-f='/Users/jaimitillo/Documents/Desarrollos/FreeCAD/src/Mod/Aircraft/naca.f'
-f2py.compile(f,'naca',-1,1)
-
